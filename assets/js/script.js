18af6613e5df27f19cb3f0ac364b73a9094e410d
Merlino79 <alessandro.ciancio@hotmail.com>
2021-04-17 21:07:51 +0200

    prova verifica funzionamento consol.log

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+//controllo del nome
+//controllare gli elementi nell'html dalla console(value)
+//somma degli elementi nel cheek
+//verificare presenza codi sconto nel coupon e in questo caso  stampo e applico lo sconto
+//stampare il prezzo 
+
+///esercizio fatto riguardando  il video  e  studiando le varie situazioni //////
+
+
+
+//referenze//
+
+var nome = "alessandro";
+console.log(nome);
